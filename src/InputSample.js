39fb7abe75f14b413dc0e32c85f6424b61873b7c
quietwave00 <quietwave00@gmail.com>
2024-04-29 15:27:04 +0900

    여러 개의 대상에 대한 useState

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,39 @@
 import React, { useState } from 'react';
 
 const InputSample = () => {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const { name, nickname } = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        console.log(e.target); // html 태그
+        console.log(e.target.name); // name 속성
+        
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value // name키
+        });
     };
 
     const onReset = () => {
-        setText('');
-    }
+        setInputs({
+            name: '',
+            nickname: '',
+        });
+    };
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input name="name" onChange={onChange} placeholder="이름" value={name}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
