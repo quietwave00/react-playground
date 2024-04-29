6faea60a548345cb54a32819f6e13ca2bd0aa979
quietwave00 <quietwave00@gmail.com>
2024-04-29 14:51:29 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import InputSample from './InputSample';
+
 
 function App() {
   return (
-    // <>
-    //   <Hello name="react" color="red"/>
-    //   <Hello color="pink"/>
-    // </>
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial />
-      <Hello color="pink"/>
-    </Wrapper>
+    <InputSample />
   );
 }
 
